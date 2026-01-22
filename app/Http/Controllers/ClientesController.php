b22fbc09b9b2a522f7aac360014c90b4fb53116c
José Henrique- <contato.henrique_@hotmail.com>
2026-01-22 12:25:48 -0300

    Fix: ClientesController refactor and restore missing columns

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 use App\Validators\CpfCnpjValidator;
 use App\Services\LogService;
 
